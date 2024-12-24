e947f0b32af48bda0cd635653bad382754173ac1
shyamsathwik <sshyamsathwik@gmail.com>
2024-12-24 19:27:07 +0530

    Got components working

---
+++ src/Player.svelte
@@ -0,0 +1,29 @@
+<style>
+  .player {
+    flex-grow: 2;
+    
+  }
+
+  .plus {
+    background-color: seagreen;
+  }
+
+  .minus {
+    background-color: brown;
+  }
+
+  .button {
+    font-size: 20px;
+    border-radius: 3px;
+    width: 40px;
+    color: white;
+    font-family: monospace;
+    font-weight: bold;
+  }
+</style>
+<div class="player">
+  <h2>20</h2>
+  <button class="plus">+</button>
+    <button class="minus">-</button>
+<h2>Blue wins</h2>
+</div>
